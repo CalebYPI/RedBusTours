487c73a2fa387cd09a1a531e2dedc8b7d3925b3c
Thokozile Snono <thokozilesinono>
2021-06-09 18:55:24 +0200

    5th commit

--- src/test/java/za/ac/cput/groupx30/factory/DriverFactoryTest.java
+++ src/test/java/za/ac/cput/groupx30/factory/DriverFactoryTest.java
@@ -1,5 +1,9 @@
 package za.ac.cput.groupx30.factory;
 
+// Author: Thokozile Snono
+// Entity: DriverFactoryTest
+// Date: 11 June 2021
+
 import org.junit.jupiter.api.Test;
 import za.ac.cput.groupx30.entity.Driver;
 import org.junit.jupiter.api.Assertions;
--- src/test/java/za/ac/cput/groupx30/factory/DriverVehicleFactoryTest.java
+++ src/test/java/za/ac/cput/groupx30/factory/DriverVehicleFactoryTest.java
@@ -1,5 +1,9 @@
 package za.ac.cput.groupx30.factory;
 
+// Author: Thokozile Snono
+// Entity: DriverVehicleFactoryTest
+// Date: 11 June 2021
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.groupx30.entity.DriverVehicle;
