10ccec6c94fe17e5194e5792f0630ebac8b2558e
Harbinger0 <shabied007@gmail.com>
2021-06-10 18:55:44 +0200

    Changes to PassengerFactory & TicketFactory

--- src/test/java/za/ac/cput/groupx30/factory/PassengerFactoryTest.java
+++ src/test/java/za/ac/cput/groupx30/factory/PassengerFactoryTest.java
@@ -4,7 +4,9 @@ package za.ac.cput.groupx30.factory;
  * Desc: TestCase for PassengerFactory
  * Date: 06 June 2021
  */
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.groupx30.entity.Passenger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,8 +14,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class PassengerFactoryTest {
 
     @Test
-    void createPassenger() {
-        Passenger passenger = PassengerFactory.createPassenger("","Shaheed", "0787092205");
-        System.out.println(passenger);
+     void createPassenger() {
+        Passenger passenger1 = PassengerFactory.createPassenger("Shaheed", "0787092205");
+        Passenger passenger2 = PassengerFactory.createPassenger("Robert", "0787547693");
+        System.out.println(passenger1 + "\n" + passenger2);
+
     }
+
+    @Test
+    @Disabled
+    void comparePassengers() {
+        Passenger passenger1 = PassengerFactory.createPassenger("Shaheed", "0787092205");
+        Passenger passenger2 = PassengerFactory.createPassenger("Robert", "0787547693");
+        System.out.println(passenger1 + " " + passenger2);
+        assertSame(passenger1, passenger2);
+
+    }
+
+
 }
--- src/test/java/za/ac/cput/groupx30/factory/TicketFactoryTest.java
+++ src/test/java/za/ac/cput/groupx30/factory/TicketFactoryTest.java
@@ -13,7 +13,7 @@ class TicketFactoryTest {
 
     @Test
     void createTicket() {
-        Ticket ticket = TicketFactory.createTicket("", "06/06/2021","15:00", "R250");
+        Ticket ticket = TicketFactory.createTicket("06/06/2021","15:00", "R250");
         System.out.println(ticket);
     }
 }
