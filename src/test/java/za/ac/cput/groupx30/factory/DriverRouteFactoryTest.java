2ab212d8483ccb61c7e08c687975065c8aac91c6
MercyRise <baronmukenyi@gmail.com>
2021-06-10 20:22:05 +0200

    minor changes

---
+++ src/test/java/za/ac/cput/groupx30/factory/DriverRouteFactoryTest.java
@@ -0,0 +1,36 @@
+package za.ac.cput.groupx30.factory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.groupx30.entity.DriverRoute;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+class DriverRouteFactoryTest {
+    DriverRoute driverRoute = DriverRouteFactory.createRoute("N1");
+
+    @Test
+    public void testRouteEqualityID(){
+        Assertions.assertEquals(driverRoute, driverRoute);
+        Assertions.assertNotNull(driverRoute);
+        Assertions.assertNotNull(driverRoute.getRouteID());
+    }
+
+    @Test
+    public void testRouteIdentity(){
+        Assertions.assertSame("N1", "N1");
+    }
+
+//    @Test
+//    @org.junit.jupiter.api.Timeout(value = 1, unit = SECONDS);
+
+    @Test
+    @Disabled
+    public void setDriverRoute(){
+
+    }
+
+}
