c6c647829c08111c4e62392a8c60665c463ec53c
Caleb Ruiters <35923446+CalebYPI@users.noreply.github.com>
2021-08-13 23:48:06 +0200

    RouteController Completed

--- src/main/java/za/ac/cput/groupx30/controller/route/RouteController.java
+++ src/main/java/za/ac/cput/groupx30/controller/route/RouteController.java
@@ -6,6 +6,8 @@ import za.ac.cput.groupx30.entity.Route;
 import za.ac.cput.groupx30.factory.RouteFactory;
 import za.ac.cput.groupx30.service.route.RouteService;
 
+import java.util.Set;
+
 @RestController()
 @RequestMapping("/route")
 public class RouteController {
@@ -19,5 +21,23 @@ public class RouteController {
         return service.create(newRoute);
     }
 
+    @RequestMapping(value = "/read/{id}", method = RequestMethod.GET)
+    public Route read(@PathVariable String id) {
+        return service.read(id);
+    }
 
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    public Route update(@RequestBody Route route) {
+        return service.update(route);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public boolean delete(@PathVariable String id) {
+        return service.delete(id);
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public Set<Route> getAll() {
+        return service.getAll();
+    }
 }
