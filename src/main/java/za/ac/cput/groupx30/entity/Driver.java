3746946e76f059bd9d384ff6c4baa564dcf44aa3
Thokozile Snono <thokozilesinono>
2021-08-30 22:53:36 +0200

    Relational DB

--- src/main/java/za/ac/cput/groupx30/entity/Driver.java
+++ src/main/java/za/ac/cput/groupx30/entity/Driver.java
@@ -4,7 +4,11 @@ package za.ac.cput.groupx30.entity;
 // Entity: Driver
 // Date: 7 June 2021
 
-public class Driver {
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class Driver implements Serializable {
     public String Id;
     public String name;
 
--- src/main/java/za/ac/cput/groupx30/repository/impl/DriverVehicleRepositoryImpl.java
+++ src/main/java/za/ac/cput/groupx30/repository/impl/DriverVehicleRepositoryImpl.java
@@ -1,65 +1,65 @@
-package za.ac.cput.groupx30.repository.impl;
-
-// Author: Thokozile Snono
-// Student number: 216032121
-// Entity: DriverVehicleRepositoryImpl
-// Date: 26 July 2021
-
-import za.ac.cput.groupx30.entity.DriverVehicle;
-import za.ac.cput.groupx30.repository.DriverVehicleRepository;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class DriverVehicleRepositoryImpl implements DriverVehicleRepository {
-    private static DriverVehicleRepository repository = null;
-    private Set< DriverVehicle > driverVehicleDB;
-
-    private DriverVehicleRepositoryImpl() { this.driverVehicleDB = new HashSet<>();}
-
-    public static DriverVehicleRepository getRepository() {
-        if (repository == null) repository = new DriverVehicleRepositoryImpl();
-        return repository;
-    }
-
-    @Override
-    public Set< DriverVehicle > getAll() {
-        return driverVehicleDB;
-    }
-
-    @Override
-    public DriverVehicle create(DriverVehicle driverVehicle) {
-        this.driverVehicleDB.add(driverVehicle);
-        return driverVehicle;
-    }
-
-    @Override
-    public DriverVehicle read(String vehicleID) {
-        for (DriverVehicle driverVehicle: driverVehicleDB) {
-            if (driverVehicle.getVehicleId().equalsIgnoreCase(vehicleID))
-                return driverVehicle;
-        }
-        return null;
-    }
-
-    @Override
-    public DriverVehicle update(DriverVehicle driverVehicle) {
-        DriverVehicle read = read(driverVehicle.getVehicleId());
-        if (read != null) {
-            this.driverVehicleDB.remove(read);
-            this.driverVehicleDB.add(driverVehicle);
-            return driverVehicle;
-        }
-        return null;
-    }
-
-    @Override
-    public boolean delete(String driverVehicleID) {
-        DriverVehicle driverVehicle = read(driverVehicleID);
-        if (driverVehicle != null) {
-            this.driverVehicleDB.remove(driverVehicleID);
-            return true;
-        }
-        return false;
-    }
-}
+//package za.ac.cput.groupx30.repository.impl;
+//
+//// Author: Thokozile Snono
+//// Student number: 216032121
+//// Entity: DriverVehicleRepositoryImpl
+//// Date: 26 July 2021
+//
+//import za.ac.cput.groupx30.entity.DriverVehicle;
+//import za.ac.cput.groupx30.repository.DriverVehicleRepository;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class DriverVehicleRepositoryImpl implements DriverVehicleRepository {
+//    private static DriverVehicleRepository repository = null;
+//    private Set< DriverVehicle > driverVehicleDB;
+//
+//    private DriverVehicleRepositoryImpl() { this.driverVehicleDB = new HashSet<>();}
+//
+//    public static DriverVehicleRepository getRepository() {
+//        if (repository == null) repository = new DriverVehicleRepositoryImpl();
+//        return repository;
+//    }
+//
+//    @Override
+//    public Set< DriverVehicle > getAll() {
+//        return driverVehicleDB;
+//    }
+//
+//    @Override
+//    public DriverVehicle create(DriverVehicle driverVehicle) {
+//        this.driverVehicleDB.add(driverVehicle);
+//        return driverVehicle;
+//    }
+//
+//    @Override
+//    public DriverVehicle read(String vehicleID) {
+//        for (DriverVehicle driverVehicle: driverVehicleDB) {
+//            if (driverVehicle.getVehicleId().equalsIgnoreCase(vehicleID))
+//                return driverVehicle;
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public DriverVehicle update(DriverVehicle driverVehicle) {
+//        DriverVehicle read = read(driverVehicle.getVehicleId());
+//        if (read != null) {
+//            this.driverVehicleDB.remove(read);
+//            this.driverVehicleDB.add(driverVehicle);
+//            return driverVehicle;
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean delete(String driverVehicleID) {
+//        DriverVehicle driverVehicle = read(driverVehicleID);
+//        if (driverVehicle != null) {
+//            this.driverVehicleDB.remove(driverVehicleID);
+//            return true;
+//        }
+//        return false;
+//    }
+//}
